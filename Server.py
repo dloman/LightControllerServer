17083a2df6c76ed36a8f457a05ad671493986bc8
Daniel Loman <dloman@toyon.com>
2014-03-02 18:13:07 -0800

    updated Server to handle color and slosh from app

--- Server.py
+++ Server.py
@@ -5,16 +5,60 @@ import web
 urls = ('/(.*)', 'hello')
 app = web.application(urls, globals())
 
+###############################################################################
+###############################################################################
 class hello:
   def GET(self, name):
     if not name:
       name = 'World'
     return 'Hello, ' + name + '!'
+
+###############################################################################
   def POST(*args,**kwargs):
-    data = web.data()
-    stuff = web.input()
-    print 'data =', data
-    print 'stuff =',stuff
+    Data  = web.input()
+    if 'Type' in Data:
+      Type = Data['Type']
+      if 'Color' in Type:
+        return GetColors(Data)
+      elif 'Slosh' in Type:
+        return GetSloshData(Data)
+      else:
+        return 'ERROR'
+
+###############################################################################
+###############################################################################
+def GetColors(Data):
+  Red   = GetFloatValue('Red',   Data)
+  Blue  = GetFloatValue('Blue',  Data)
+  Green = GetFloatValue('Green', Data)
+  Alpha = GetFloatValue('Alpha', Data)
+  print 'Red =',   Red
+  print 'Blue =',  Blue
+  print 'Green =', Green
+  print 'Alpha =', Alpha
+  return Red, Blue, Green, Alpha
+
+###############################################################################
+def GetFloatValue(Float, Data):
+  if Float in Data:
+    try:
+      return float(Data[Float])
+    except:
+      return None
+
+###############################################################################
+def GetBool(Value, Data):
+  if Value in Data:
+    return bool(Data[Value])
+  else:
+    return False
+
+###############################################################################
+def GetSloshData(Data):
+  Horizontal = GetBool('Horizontal', Data)
+  Vertical   = GetBool('Vertical'  , Data)
+  Freq       = GetFloat('Frequency', Data)
+
 
 if __name__ == "__main__":
   app.run()
